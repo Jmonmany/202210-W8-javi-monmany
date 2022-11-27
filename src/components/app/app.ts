3d9e05b129c893ded2ee63b426f6798a6e740a17
Javi <monmany1992@gmail.com>
2022-11-27 17:39:41 +0100

    Added components and functions to render series and stars

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -1,10 +1,10 @@
-import { Header } from "../header/header.js";
-import { List } from "../lists/lists.js";
-import { Main } from "../main/main.js";
+import { Header } from '../header/header.js';
+import { List } from '../lists/lists.js';
+import { Main } from '../main/main.js';
 
 export class App {
   constructor() {
-     new Header('.container');
-     new Main('slot');
+    new Header('.container');
+    new Main('slot');
   }
 }
